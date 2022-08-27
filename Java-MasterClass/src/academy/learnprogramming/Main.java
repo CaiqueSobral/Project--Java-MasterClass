951f8d994448835e49d678dad227cd496185abfb
Caique Sobral <caiquelsobral@gmail.com>
2022-08-27 20:04:01 -0300

    FEAT: Primitive tpes

---
+++ Java-MasterClass/src/academy/learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+  public static void main(String[] args) {
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+    
+    System.out.println("Integer Minimum value = " + myMinIntValue);
+    System.out.println("Integer Minimum value = " + myMaxIntValue);
+
+    int myMaxIntTest = 2_456_034;
+    System.out.println(myMaxIntTest);
+  }
+}
