2a3c36a59bc68a8b0479ed8f42539c33f9a471d8
Caique Sobral <caiquelsobral@gmail.com>
2022-09-22 10:07:13 -0300

    FEAT: reading files with java

---
+++ src/com/inter/hFiles/application/Program.java
@@ -0,0 +1,27 @@
+package com.inter.hFiles.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        File file = new File("H:\\Files\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
