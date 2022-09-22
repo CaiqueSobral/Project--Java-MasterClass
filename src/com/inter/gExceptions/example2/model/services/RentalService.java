2fe056ec4abf61b292e1408e43f5f037ae6f65f3
Caique Sobral <caiquelsobral@gmail.com>
2022-09-22 13:23:27 -0300

    FEAT: implement solution with interface

---
+++ src/com/inter/gExceptions/example2/model/services/RentalService.java
@@ -0,0 +1,36 @@
+package com.inter.gExceptions.example2.model.services;
+
+import com.inter.gExceptions.example2.model.entities.CarRental;
+import com.inter.gExceptions.example2.model.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private TaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+
+        double hour = (double) (t2 - t1) / 1000 / 60 / 60;
+        double basicPayment;
+
+        if (hour <= 12.0) {
+            basicPayment = Math.ceil(hour) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hour / 24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
---
+++ src/com/inter/gExceptions/example2/model/services/TaxService.java
@@ -0,0 +1,6 @@
+package com.inter.gExceptions.example2.model.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+}
