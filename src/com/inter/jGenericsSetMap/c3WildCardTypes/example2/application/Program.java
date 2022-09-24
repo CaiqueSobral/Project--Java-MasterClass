43eb6600bc13e860278dd61333756f00b1e3d999
Caique Sobral <caiquelsobral@gmail.com>
2022-09-24 13:15:54 -0300

    FEAT: Using wild card types into functions

--- src/com/inter/jGenericsSetMap/c3WildCardTypes/example2/application/Program.java
+++ src/com/inter/jGenericsSetMap/c3WildCardTypes/example2/application/Program.java
@@ -1,4 +1,24 @@
 package com.inter.jGenericsSetMap.c3WildCardTypes.example2.application;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Program {
+
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+
+        List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+        printList(myStrs);
+    }
+
+
+    // With the wildcard type, I can print any type of list (Integer, String, Double)
+    public static void printList(List<?> list) {
+        //list.add(3); // Error, can't add data into wildcard list
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
 }
